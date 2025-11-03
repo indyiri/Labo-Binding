02a814041e7233d79438138809ec46b262d33db9
Indy Iri <12402665@student.pxl.be>
2025-11-03 15:17:57 +0100

    bijna afgewerkt

---
+++ ActorsApplication/Converters/AgeToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorsApplication.Converters
+{
+    public class AgeToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            int birthYear = (int)value;
+
+            int currentYear = DateTime.Now.Year;
+
+            int age = currentYear - birthYear;
+
+            if (age < 50)
+            {
+                return Colors.Blue;
+            }
+            else
+            {
+                return Colors.Green;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
