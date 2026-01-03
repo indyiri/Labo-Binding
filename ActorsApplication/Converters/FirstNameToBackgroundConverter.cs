491cce614b48e7cb65742b8db370f0bbc19c7256
Indy Iri <12402665@student.pxl.be>
2026-01-03 22:41:41 +0100

    Deel 6 afgewerkt

---
+++ ActorsApplication/Converters/FirstNameToBackgroundConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ActorsApplication.Converters
+{
+    public class FirstNameToBackgroundConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null) return Colors.Transparent;
+
+            string firstName = value.ToString() ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(firstName))
+                return Colors.Transparent;
+
+            char first = char.ToUpperInvariant(firstName[0]);
+
+            // A-I -> gray, J-Z -> light blue
+            if (first >= 'A' && first <= 'I')
+                return Colors.LightGray;
+
+            return Colors.LightBlue;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
